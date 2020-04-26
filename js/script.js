f27672168410a09b71b2e8ee2b0a53b09d38dc3d
kenan <kenabdulka@gmail.com>
2020-04-26 14:48:31 -0700

    created google api key and used it to load city view using city name from user input

--- js/script.js
+++ js/script.js
@@ -18,4 +18,13 @@ function loadData() {
     var apiKey = "AIzaSyCebedLJ9rvIByNJrQJAGv-y47zLIlf35k";
     alert("printing test for street Input " + address);
 
+    var imgElement = $("<img>");
+    var imgSrcUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '&key=AIzaSyCebedLJ9rvIByNJrQJAGv-y47zLIlf35k';
+    // $body.append('<img class="bgimg" src="' + imgSrcUrl + '">');
+    $body.append(imgElement);
+    imgElement.addClass("bgimg");
+    imgElement.attr("src",  imgSrcUrl);
+   
+    $nytHeaderElem.text("New York Times Articles about " + cityInput);
+
 };
