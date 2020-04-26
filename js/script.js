b555b1c64a0fe72045a571b16fbfa65b35ba4807
kenan <kenabdulka@gmail.com>
2020-04-26 14:42:24 -0700

    added script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    
+
+};
